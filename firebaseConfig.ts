6d85bc8cc2ba42c3493f7acbd8c1ae87da2df348
anamitraburhagohain-dot <anamitraburhagohain@gmail.com>
2026-01-21 00:13:17 +0530

    Add files via upload

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -8,11 +8,11 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyDVSqnEe1o57urBRrMKGiY_IOyOLmRSVw4",
   authDomain: "teenpatti-b626a.firebaseapp.com",
-  databaseURL: "https://teenpatti-b626a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://teenpatti-b626a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "teenpatti-b626a",
   storageBucket: "teenpatti-b626a.firebasestorage.app",
   messagingSenderId: "687205335645",
-  appId: "1:687205335645:web:d11951f51c3a7b5fdc03b4"
+  appId: "1:687205335645:web:bb1bc61b6d5ee58fdc03b4"
 };
 
 // Initialize Firebase
